9632f426a6a36c375e3d2f23ae19263d77421b38
Heo_IOS <heowork4327@gmail.com>
2022-11-28 15:42:33 +0900

    Initial Commit

---
+++ MyBeaconTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBeaconTest
+//
+//  Created by nyeong heo on 2022/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
